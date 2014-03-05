992886ce79d314fae4693cc293be6a69017c0611
Rachel Seale <rseale@micros.com>
2014-03-05 15:21:31 +0000

    php tryout again

--- mailer.php
+++ mailer.php
@@ -1,63 +1,25 @@
-<?php
-/* Set e-mail recipient */
-$myemail = "rvseale@sky.com";
+<?php if (isset($_POST['message'])) {
 
-/* Check all form inputs using check_input function */
-$name = check_input($_POST['name'], "Enter your name");
-$subject = check_input($_POST['subject'], "Enter a subject");
-$email = check_input($_POST['email']);
-$message = check_input($_POST['message'], "Write your message");
+   $name = (isset($_POST['name'])) ? $_POST['name'] : "" ;
+   $email = (isset($_POST['email'])) ? $_POST['email'] : "" ;
+   $message = (isset($_POST['message'])) ? $_POST['message'] : "" ;
+   $honey = (isset($_POST['username'])) ? $_POST['username'] : "" ;
 
-/* If e-mail is not valid show error message */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
-{
-show_error("E-mail address not valid");
-}
-/* Let's prepare the message for the e-mail */
-$message = "
+   if($name !== "" && $email !== "" && $message !== "" && $honey == ""){   
+    $to = "rvseale@msky.com";
+    $subject = "rachelseale.co.uk query from ".$name." (".$email.")";
+    $body = "To Rachel\n\n".$message."\n\nThanks,\n\n".$name.".";
+    $headers = "From: ".$email;
+    if(mail($to, $subject, $body, $headers)){
+     echo "<p>Thank you $name, your message has been sent successfully.</p>";
+    }else{
+     echo "<p class='error'>Sorry $name. Message delivery failed. Please try again.</p>";
+    }
+   }else{
+    echo "<p class='error'>Sorry but the form failed to validate please <a href='/contact'>try again</a> remembering to fill in all the fields, and using a valid email address.</p>";
+   }
+  }else{
+   echo '<p>Please feel free to contact me with any questions or feedback you may have. You can tweet me <a href="http://twitter.com/RachelSeale">@RachelSeale</a> or use this handy contact form below:</p>';
+  } 
 
-Name: $name
-E-mail: $email
-Subject: $subject
-
-Message:
-$message
-
-";
-
-/* Send the message using mail() function */
-mail($myemail, $subject, $message);
-
-/* Redirect visitor to the thank you page */
-header('Location: thanks.html');
-exit();
-
-/* Functions we used */
-function check_input($data, $problem='')
-{
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-if ($problem && strlen($data) == 0)
-{
-show_error($problem);
-}
-return $data;
-}
-
-function show_error($myError)
-{
-?>
-<html>
-<body>
-
-<p>Please correct the following error:</p>
-<strong><?php echo $myError; ?></strong>
-<p>Hit the back button and try again</p>
-
-</body>
-</html>
-<?php
-exit();
-}
 ?>
