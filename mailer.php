43e7b9445cacfb8f02825613c2b9734a83ee5a85
Rachel Seale <rseale@micros.com>
2014-03-05 14:43:14 +0000

    Php update

--- mailer.php
+++ mailer.php
@@ -1,63 +1,24 @@
 <?php
-/* Set e-mail recipient */
-$myemail = "rvseale@sky.com";
 
-/* Check all form inputs using check_input function */
-$name = check_input($_POST['name'], "Enter your name");
-$subject = check_input($_POST['subject'], "Enter a subject");
-$email = check_input($_POST['email']);
-$message = check_input($_POST['message'], "Write your message");
+// From 
+$header="from: $name <$mail_from>";
 
-/* If e-mail is not valid show error message */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
-{
-show_error("E-mail address not valid");
+// Details
+$email="$email";
+
+// Mail of sender
+$mail_from="$customer_mail"; 
+
+// Enter your email address
+$to ='rvseale@sky.com';
+$send_contact=mail($to, $header, $email, $customer_mail);
+
+// Check, if message sent to your email 
+// display message "We've recived your information"
+if($send_contact){
+echo "We've recived your contact information";
 }
-/* Let's prepare the message for the e-mail */
-$message = "
-
-Name: $name
-E-mail: $email
-Subject: $subject
-
-Message:
-$message
-
-";
-
-/* Send the message using mail() function */
-mail($myemail, $subject, $message);
-
-/* Redirect visitor to the thank you page */
-header('Location: thanks.html');
-exit();
-
-/* Functions we used */
-function check_input($data, $problem='')
-{
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-if ($problem && strlen($data) == 0)
-{
-show_error($problem);
-}
-return $data;
-}
-
-function show_error($myError)
-{
-?>
-<html>
-<body>
-
-<p>Please correct the following error:</p>
-<strong><?php echo $myError; ?></strong>
-<p>Hit the back button and try again</p>
-
-</body>
-</html>
-<?php
-exit();
+else {
+echo "ERROR";
 }
 ?>
